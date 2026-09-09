84758c911c5373a4d8a4bdd81c7cce01acbb3dda
waxew <4622.ar@gmail.com>
2026-09-09 18:27:38 +0330

    fix: align AddCustomerUseCase with Customer model package

--- core/domain/src/main/java/com/wcrm/core/domain/usecase/customer/AddCustomerUseCase.kt
+++ core/domain/src/main/java/com/wcrm/core/domain/usecase/customer/AddCustomerUseCase.kt
@@ -1,7 +1,7 @@
 package com.wcrm.core.domain.usecase.customer
 
 import com.wcrm.core.domain.repository.CustomerRepository
-import com.wcrm.core.model.Customer
+import com.wcrm.core.model.customer.Customer
 
 /**
  * مورد استفاده افزودن مشتری جدید به CRM.
