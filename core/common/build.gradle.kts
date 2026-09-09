df068ff5e0038d5032dd852be33eeaddbb335462
waxew <4622.ar@gmail.com>
2026-09-09 18:38:16 +0330

    fix: configure common as android library module

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,5 +1,15 @@
 plugins {
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.wcrm.core.common"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+    }
 }
 
 kotlin {
