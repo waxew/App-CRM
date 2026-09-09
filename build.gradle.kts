73d72923107baf8b0a0d47b41bbd32f1e720f3dc
waxew <4622.ar@gmail.com>
2026-09-09 17:35:52 +0330

    Add root Gradle Kotlin DSL configuration

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// Root Gradle build configuration for W-CRM
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("com.android.library") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
+}
