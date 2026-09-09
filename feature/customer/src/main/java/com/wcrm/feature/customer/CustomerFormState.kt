f1891fa66df7cbef74ceb1346cc2a3c41887fc6c
waxew <4622.ar@gmail.com>
2026-09-09 17:50:32 +0330

    Add customer form state foundation

---
+++ feature/customer/src/main/java/com/wcrm/feature/customer/CustomerFormState.kt
@@ -0,0 +1,8 @@
+package com.wcrm.feature.customer
+
+ data class CustomerFormState(
+    val name: String = "",
+    val phone: String = "",
+    val email: String = "",
+    val notes: String = ""
+)
