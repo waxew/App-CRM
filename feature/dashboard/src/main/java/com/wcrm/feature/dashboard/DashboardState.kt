373d2f4ab0ee77bf737dbf823a6498f216fba7b5
waxew <4622.ar@gmail.com>
2026-09-09 17:50:39 +0330

    Add dashboard state model

---
+++ feature/dashboard/src/main/java/com/wcrm/feature/dashboard/DashboardState.kt
@@ -0,0 +1,8 @@
+package com.wcrm.feature.dashboard
+
+ data class DashboardState(
+    val todaySales: Double = 0.0,
+    val customerCount: Int = 0,
+    val inventoryCount: Int = 0,
+    val revenue: Double = 0.0
+)
