4fc489dbca1c2385ca46a7114008b3eff7df98a9
waxew <4622.ar@gmail.com>
2026-09-09 17:45:00 +0330

    Add InvoiceRepositoryImpl foundation

---
+++ core/repository/src/main/java/com/wcrm/core/repository/invoice/InvoiceRepositoryImpl.kt
@@ -0,0 +1,8 @@
+package com.wcrm.core.repository.invoice
+
+import com.wcrm.core.domain.repository.InvoiceRepository
+import javax.inject.Inject
+
+class InvoiceRepositoryImpl @Inject constructor() : InvoiceRepository {
+    // Invoice data operations will be connected to Room InvoiceDao.
+}
