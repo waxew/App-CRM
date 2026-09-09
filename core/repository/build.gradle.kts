0df17ff3186e67e35ed8d930712d14eed94a6213
waxew <4622.ar@gmail.com>
2026-09-09 18:38:37 +0330

    fix: enable Hilt in repository module

--- core/repository/build.gradle.kts
+++ core/repository/build.gradle.kts
@@ -1,15 +1,28 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
     namespace = "com.wcrm.core.repository"
     compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
     implementation(project(":core:domain"))
     implementation(project(":core:database"))
     implementation(project(":core:model"))
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
 }
