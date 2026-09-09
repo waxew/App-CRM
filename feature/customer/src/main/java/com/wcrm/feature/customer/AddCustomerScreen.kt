a62732d8fcc981a629d91297cfb0a619469f95c4
waxew <4622.ar@gmail.com>
2026-09-09 17:51:44 +0330

    Add customer form compose foundation

---
+++ feature/customer/src/main/java/com/wcrm/feature/customer/AddCustomerScreen.kt
@@ -0,0 +1,11 @@
+package com.wcrm.feature.customer
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AddCustomerScreen(
+    state: CustomerFormState = CustomerFormState()
+) {
+    // Customer form UI foundation.
+    // Fields and validation will be connected to ViewModel.
+}
