d6e79f0f12cc1eedab7d702dbc0c5a170fffd3a8
waxew <4622.ar@gmail.com>
2026-09-09 17:46:05 +0330

    Add customer compose screen foundation

---
+++ feature/customer/src/main/java/com/wcrm/feature/customer/CustomerScreen.kt
@@ -0,0 +1,9 @@
+package com.wcrm.feature.customer
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomerScreen() {
+    Text(text = "Customers")
+}
