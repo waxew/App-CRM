2b727d300e97cf2678b1eb635d098d8405464ad7
waxew <4622.ar@gmail.com>
2026-09-09 17:42:37 +0330

    Add Hilt DatabaseModule foundation

---
+++ core/database/src/main/java/com/wcrm/core/database/di/DatabaseModule.kt
@@ -0,0 +1,11 @@
+package com.wcrm.core.database.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    // Room provider will be added here.
+}
