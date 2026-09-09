091a59288d6fb8416076b86c770e2202450a73ac
waxew <4622.ar@gmail.com>
2026-09-09 17:44:55 +0330

    Add ProductRepositoryImpl foundation

---
+++ core/repository/src/main/java/com/wcrm/core/repository/product/ProductRepositoryImpl.kt
@@ -0,0 +1,8 @@
+package com.wcrm.core.repository.product
+
+import com.wcrm.core.domain.repository.ProductRepository
+import javax.inject.Inject
+
+class ProductRepositoryImpl @Inject constructor() : ProductRepository {
+    // Product data operations will be connected to Room ProductDao.
+}
