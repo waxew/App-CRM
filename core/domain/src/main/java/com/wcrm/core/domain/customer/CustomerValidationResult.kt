232f6476c4123f485247d186f52f5a0eb85ed772
waxew <4622.ar@gmail.com>
2026-09-09 18:10:51 +0330

    Add Persian documentation to CustomerValidationResult domain model

--- core/domain/src/main/java/com/wcrm/core/domain/customer/CustomerValidationResult.kt
+++ core/domain/src/main/java/com/wcrm/core/domain/customer/CustomerValidationResult.kt
@@ -1,6 +1,22 @@
 package com.wcrm.core.domain.customer
 
+/**
+ * نتیجه اعتبارسنجی اطلاعات مشتری.
+ *
+ * این مدل در لایه Domain قرار دارد تا منطق اعتبارسنجی
+ * مستقل از رابط کاربری و دیتابیس نگهداری شود.
+ */
 sealed interface CustomerValidationResult {
+
+    /**
+     * زمانی که اطلاعات مشتری معتبر است.
+     */
     data object Valid : CustomerValidationResult
+
+    /**
+     * زمانی که اطلاعات وارد شده دارای خطا است.
+     *
+     * @param message پیام توضیح خطای اعتبارسنجی
+     */
     data class Invalid(val message: String) : CustomerValidationResult
 }
