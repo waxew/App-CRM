77a8cd5b6f6a3afab166f7d513606aa56350b2d7
waxew <4622.ar@gmail.com>
2026-09-09 17:47:34 +0330

    Add customer viewmodel factory placeholder

---
+++ feature/customer/src/main/java/com/wcrm/feature/customer/CustomerViewModelFactory.kt
@@ -0,0 +1,7 @@
+package com.wcrm.feature.customer
+
+/**
+ * Placeholder for Hilt ViewModel integration.
+ * The final implementation will bind CustomerViewModel through Hilt.
+ */
+class CustomerViewModelFactory
