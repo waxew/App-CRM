2d55610d74e7a66422ca6636bd33640be67e7917
waxew <4622.ar@gmail.com>
2026-09-09 17:45:05 +0330

    Add RepositoryModule foundation for Hilt

---
+++ core/repository/src/main/java/com/wcrm/core/repository/di/RepositoryModule.kt
@@ -0,0 +1,11 @@
+package com.wcrm.core.repository.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+    // Repository bindings will be added here.
+}
